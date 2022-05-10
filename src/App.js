83d8b1c96ae23a796e4b6cf305474d87617661de
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-05-10 08:35:33 +0500

    installed the react-router-dom and started a serious work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  return <div className='app'>
-    <h1>Let's build the Google Clone🚀🚀🚀</h1>
-  </div>
+  return (
+    <div className='app'>
+      <h1>Let's build the Google Clone🚀🚀🚀</h1>
+      <Home />
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+      <h1>I'm a home page</h1>
+
+      <div className='home__header'>
+        <div className='home__header-left'>
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className='home__header-right'>
+          {/* Link */}
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+
+      <div className='home__body'></div>
+    </div>
+  )
+}
+
+export default Home
